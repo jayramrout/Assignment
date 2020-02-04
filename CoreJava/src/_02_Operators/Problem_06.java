5862512d838c8d06bb7a85ad7fe81d24b699a954
Jayram <jayram.rout@hotmail.com>
2020-02-03 23:23:46 -0500

    Updates

---
+++ CoreJava/src/_02_Operators/Problem_06.java
@@ -0,0 +1,19 @@
+package _02_Operators;
+
+/**
+ * Using ternary Operator check which number is maximum and print it.
+ */
+public class Problem_06 {
+	public static void main(String[] args) {
+		int a=0; // Use scanner to take the input for a
+		int b=0; // Use scanner to take input for b
+
+		int max = 0; // use the logic here to assign the max value using ternary operator
+		System.out.println("Max of both number is "+ max);
+
+		int min = 0; // use the logic here to assign the max value using ternary operator
+		System.out.println("Min of both number is "+ min);
+
+	}
+
+}
